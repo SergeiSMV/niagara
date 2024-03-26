19b33be16d8645fad01ecbcb05ec0a676fb16c4b
Oleg Tsarenkoff <tsarenkoff.work@gmail.com>
2024-03-26 06:52:32 +0300

    add test

--- test/features/splash/splash_ui_test.dart
+++ test/features/splash/splash_ui_test.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: omit_local_variable_types
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lottie/lottie.dart';
 import 'package:mockito/mockito.dart';
+import 'package:niagara_app/core/utils/constants/app_constants.dart';
 import 'package:niagara_app/features/splash/presentation/cubit/splash_cubit.dart';
 import 'package:niagara_app/features/splash/presentation/pages/splash_page.dart';
 
@@ -35,6 +38,30 @@ void main() {
       // Ожидаем, что найден один виджет Positioned
       expect(find.byType(Positioned), findsOneWidget);
     });
+  });
+
+  testWidgets('test SplashPage opacity animation handling',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BlocProvider<SplashCubit>(
+          create: (context) => MockSplashCubit(),
+          child: const SplashPage(),
+        ),
+      ),
+    );
+
+    // Инициализация со значением 0
+    expect(find.byType(AnimatedOpacity), findsWidgets);
+    final AnimatedOpacity animatedOpacity =
+        tester.widget(find.byType(AnimatedOpacity).first);
+    expect(animatedOpacity.opacity, 0);
+
+    // После анимации значение 1
+    await tester.pump(AppConst.kSplashLogoDuration);
+    final AnimatedOpacity animatedOpacityAfter =
+        tester.widget(find.byType(AnimatedOpacity).first);
+    expect(animatedOpacityAfter.opacity, 1);
   });
 
   test('initial state is false', () {
