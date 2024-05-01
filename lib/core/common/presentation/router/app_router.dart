e9b60a35775ef202be094a15b755f63f6e734643
Oleg Tsarenkoff <tsarenkoff.work@gmail.com>
2024-05-01 17:08:51 +0300

    Refactor router classes and remove unused imports

--- lib/core/common/presentation/router/app_router.dart
+++ lib/core/common/presentation/router/app_router.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:niagara_app/core/common/presentation/router/app_router.gr.dart';
 import 'package:niagara_app/core/common/presentation/router/routers/auth_routes.dart';
@@ -37,14 +36,5 @@ class AppRouter extends $AppRouter {
 
   /// Анимация переходов между экранами приложения.
   @override
-  RouteType get defaultRouteType {
-    return RouteType.custom(
-      transitionsBuilder: (_, animation, __, child) {
-        return FadeTransition(
-          opacity: animation,
-          child: child,
-        );
-      },
-    );
-  }
+  RouteType get defaultRouteType => const RouteType.adaptive();
 }
