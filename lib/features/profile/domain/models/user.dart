762a32c0043f53fc00bd8e84d4edae2b5836043e
Oleg Tsarenkoff <tsarenkoff.work@gmail.com>
2024-05-01 11:49:15 +0300

    Add new models for bonuses and user profiles

---
+++ lib/features/profile/domain/models/user.dart
@@ -0,0 +1,41 @@
+import 'package:niagara_app/core/core.dart';
+
+class User extends Equatable {
+  const User({
+    required this.id,
+    required this.userId,
+    required this.name,
+    required this.surname,
+    required this.patronymic,
+    required this.login,
+    required this.phone,
+    required this.email,
+    required this.birthday,
+    required this.defaultLocationId,
+  });
+
+  final int id;
+  final String userId;
+  final String name;
+  final String surname;
+  final String patronymic;
+  final String login;
+  final String phone;
+  final String email;
+  final String birthday;
+  final String defaultLocationId;
+
+  @override
+  List<Object> get props => [
+        id,
+        userId,
+        name,
+        surname,
+        patronymic,
+        login,
+        phone,
+        email,
+        birthday,
+        defaultLocationId,
+      ];
+}
