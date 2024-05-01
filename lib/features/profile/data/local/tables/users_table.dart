d99b97dcbf2247947a435715bd0c66b9ca2b9331
Oleg Tsarenkoff <tsarenkoff.work@gmail.com>
2024-05-01 11:50:18 +0300

    Add user-related database functionality and mappers

---
+++ lib/features/profile/data/local/tables/users_table.dart
@@ -0,0 +1,17 @@
+import 'package:drift/drift.dart';
+
+class UsersTable extends Table {
+  IntColumn get id => integer()();
+  TextColumn get userId => text()();
+  TextColumn get name => text()();
+  TextColumn get surname => text()();
+  TextColumn get patronymic => text()();
+  TextColumn get login => text()();
+  TextColumn get phone => text()();
+  TextColumn get email => text()();
+  TextColumn get birthday => text()();
+  TextColumn get defaultLocationId => text()();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
