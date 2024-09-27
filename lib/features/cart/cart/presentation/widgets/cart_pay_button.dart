eb7888657d0c62b19bdf4a59af06bc4eeeda3052
Kirill Bykov <amfeek3@gmail.com>
2024-09-27 09:12:55 +0200

    [NIAGARA-359] Исправлено: пользователь мог попытаться оформить заказ без выбранного адреса, что привело бы к багам

--- lib/features/cart/cart/presentation/widgets/cart_pay_button.dart
+++ lib/features/cart/cart/presentation/widgets/cart_pay_button.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:niagara_app/core/common/presentation/widgets/buttons/app_text_button.dart';
+import 'package:niagara_app/core/common/presentation/widgets/snack_bars/app_snack_bar.dart';
 import 'package:niagara_app/core/utils/constants/app_borders.dart';
+import 'package:niagara_app/core/utils/constants/app_boxes.dart';
 import 'package:niagara_app/core/utils/constants/app_constants.dart';
 import 'package:niagara_app/core/utils/constants/app_insets.dart';
 import 'package:niagara_app/core/utils/constants/app_sizes.dart';
@@ -26,10 +29,45 @@ class PayButton extends StatelessWidget {
   /// Текст кнопки.
   final String text;
 
-  bool get _inactive => cart.products.isEmpty;
+  bool get _noAddress => cart.locationId.isEmpty;
+  bool get _inactive => cart.products.isEmpty || _noAddress;
 
-  void _goToOrderPlacing(BuildContext context) =>
-      context.pushRoute(redirectRoute);
+  void _proceedToPayment(BuildContext context) {
+    if (cart.minLimit.minAmountLeft > 0) {
+      _showMinimumOrderAmountError(context);
+    } else {
+      // TODO: Это отображает модальное окно с предложением перенести
+      // отсутствующие товары в избранное. Пока решили не реализовывать.
+      // if (cart.unavailableProducts.isNotEmpty) {
+      //   _showUnavailableProductsToFavorites(context);
+      //   return;
+      // }
+      _showMinimumOrderAmountError(context);
+      // context.pushRoute(redirectRoute);
+    }
+  }
+
+  /// Отображает модальное окно с предложением перенести отсутствующие товары в
+  /// избранное.
+  void _showUnavailableProductsToFavorites(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: context.colors.mainColors.white,
+      builder: (context) {
+        return const UnavailableProductsToFavoritesWidget();
+      },
+    );
+  }
+
+  /// Отображает сообщение об ошибке, если минимальная сумма заказа не
+  /// достигнута.
+  void _showMinimumOrderAmountError(BuildContext context) {
+    AppSnackBar.showError(
+      context,
+      title: t.cart.minimumOrderAmount(n: cart.minLimit.minAmount.round()),
+      subtitle: t.cart.needToSelectMore(n: cart.minLimit.minAmountLeft.round()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +88,9 @@ class PayButton extends StatelessWidget {
             AppInsets.kVertical12 +
             AppInsets.kBottom12,
         child: InkWell(
-          onTap: _inactive ? null : () => _goToOrderPlacing(context),
+          onTap: (_inactive || _noAddress)
+              ? null
+              : () => _proceedToPayment(context),
           child: Container(
             alignment: Alignment.center,
             padding: AppInsets.kHorizontal16,
@@ -63,27 +103,76 @@ class PayButton extends StatelessWidget {
             height: AppSizes.kButtonLarge,
             width: double.infinity,
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  t.product(n: cart.products.length),
-                  style: context.textStyle.textTypo.tx2Medium
-                      .withColor(context.colors.textColors.white),
-                ),
-                Text(
-                  text,
-                  style: context.textStyle.buttonTypo.btn1bold
-                      .withColor(context.colors.textColors.white),
-                ),
-                Text(
-                  '${cart.cartData.totalPrice.round()} ${t.common.rub}',
-                  style: context.textStyle.textTypo.tx2Medium
-                      .withColor(context.colors.textColors.white),
-                ),
-              ],
+              mainAxisAlignment: _noAddress
+                  ? MainAxisAlignment.center
+                  : MainAxisAlignment.spaceBetween,
+              children: _noAddress
+                  ? [
+                      Text(
+                        t.locations.enterAddress,
+                        style: context.textStyle.buttonTypo.btn1bold
+                            .withColor(context.colors.textColors.white),
+                      ),
+                    ]
+                  : [
+                      Text(
+                        t.product(n: cart.products.length),
+                        style: context.textStyle.textTypo.tx2Medium
+                            .withColor(context.colors.textColors.white),
+                      ),
+                      Text(
+                        text,
+                        style: context.textStyle.buttonTypo.btn1bold
+                            .withColor(context.colors.textColors.white),
+                      ),
+                      Text(
+                        '${cart.cartData.totalPrice.round()} ${t.common.rub}',
+                        style: context.textStyle.textTypo.tx2Medium
+                            .withColor(context.colors.textColors.white),
+                      ),
+                    ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class UnavailableProductsToFavoritesWidget extends StatelessWidget {
+  const UnavailableProductsToFavoritesWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: AppInsets.kAll16 + AppInsets.kVertical8,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            t.cart.notAvailablePresent,
+            style: context.textStyle.headingTypo.h3,
+          ),
+          AppBoxes.kHeight8,
+          Text(
+            t.cart.addToFavoritesSuggestion,
+            style: context.textStyle.textTypo.tx1Medium.withColor(
+              context.colors.textColors.secondary,
+            ),
+          ),
+          AppBoxes.kHeight24,
+          AppTextButton.accent(
+            text: t.cart.transferAndProceed,
+            onTap: () {},
+          ),
+          AppBoxes.kHeight12,
+          AppTextButton.secondary(
+            text: t.cart.proceedWithoutTransfer,
+            onTap: () {},
+          ),
+        ],
       ),
     );
   }
