abf1eb3ccfef1af80c59405b592a3f02afb66833
Kirill Bykov <amfeek3@gmail.com>
2024-09-30 14:24:39 +0200

    [NIAGARA-352] Добавлен баннер бонусной программы

--- lib/features/profile/user/presentation/widgets/banners_widget.dart
+++ lib/features/profile/user/presentation/widgets/banners_widget.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:niagara_app/core/common/presentation/router/app_router.gr.dart';
 import 'package:niagara_app/core/utils/constants/app_boxes.dart';
@@ -35,6 +36,13 @@ class BannersWidget extends StatelessWidget {
       stops: AppConstants.profileBannersStops,
     );
 
+    final LinearGradient bonusesGradient = LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: context.colors.gradientColors.bonusesBanner,
+      stops: AppConstants.profileBannersStops,
+    );
+
     return Padding(
       padding: AppInsets.kVertical12 + AppInsets.kHorizontal16,
       child: Column(
@@ -65,6 +73,16 @@ class BannersWidget extends StatelessWidget {
             title: t.profile.banners.vipHeader,
             description: t.profile.banners.vipDescription,
             image: Assets.images.vIPBannerImage,
+            bottomPositioning: -25,
+            rightPositioning: -25,
+          ),
+          AppBoxes.kHeight12,
+          BannerWidget(
+            onTap: () => context.navigateTo(const AboutBonusesRoute()),
+            gradient: bonusesGradient,
+            title: t.profile.banners.bonusesHeader,
+            description: t.profile.banners.bonusesDescription,
+            image: Assets.images.bonusesBanner,
             bottomPositioning: -25,
             rightPositioning: -25,
           ),
