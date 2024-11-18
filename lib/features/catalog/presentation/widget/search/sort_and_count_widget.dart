faefc39d0c14910475f8d86ddd1b75c44a4689e0
Kirill Bykov <amfeek3@gmail.com>
2024-11-18 20:19:40 +0100

    [NIAGARA-467] Исправлен виджет "Найдено n товаров"

--- lib/features/catalog/presentation/widget/search/sort_and_count_widget.dart
+++ lib/features/catalog/presentation/widget/search/sort_and_count_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:niagara_app/core/utils/constants/app_boxes.dart';
 import 'package:niagara_app/core/utils/constants/app_insets.dart';
+import 'package:niagara_app/core/utils/extensions/build_context_ext.dart';
 import 'package:niagara_app/core/utils/gen/strings.g.dart';
 import 'package:niagara_app/features/catalog/presentation/bloc/catalog_search_bloc/catalog_search_bloc.dart';
 import 'package:niagara_app/features/catalog/presentation/widget/search/sort_search_button_widget.dart';
@@ -13,7 +14,7 @@ class SortAndCountWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<CatalogSearchBloc, CatalogSearchState>(
       builder: (_, state) => state.maybeWhen(
-        loaded: (products) => Padding(
+        loaded: (products, totalCount) => Padding(
           padding: AppInsets.kHorizontal16 + AppInsets.kVertical8,
           child: Row(
             children: [
@@ -21,7 +22,8 @@ class SortAndCountWidget extends StatelessWidget {
               AppBoxes.kWidth8,
               Expanded(
                 child: Text(
-                  '${t.catalog.foundProducts} ${t.product(n: products.length)}',
+                  '${t.catalog.foundProducts} ${t.product(n: totalCount)}',
+                  style: context.textStyle.textTypo.tx2Medium,
                   textAlign: TextAlign.center,
                 ),
               ),
