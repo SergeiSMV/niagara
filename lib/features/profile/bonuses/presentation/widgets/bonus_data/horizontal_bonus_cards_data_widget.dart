b0b871ca7d886913bc12b84b3b0493a4171b7d01
Sergei Semenov <sergeyssbuh@gmail.com>
2025-03-06 19:07:29 +0300

    [NIAGARA-501] temporary bonuses redesign

--- lib/features/profile/bonuses/presentation/widgets/bonus_data/horizontal_bonus_cards_data_widget.dart
+++ lib/features/profile/bonuses/presentation/widgets/bonus_data/horizontal_bonus_cards_data_widget.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:niagara_app/core/utils/constants/app_boxes.dart';
+import 'package:niagara_app/core/utils/constants/app_sizes.dart';
+import 'package:niagara_app/features/profile/bonuses/domain/models/bonuses.dart';
 import 'package:niagara_app/features/profile/bonuses/presentation/widgets/bonus_data/bonuses_data_widget.dart';
 import 'package:niagara_app/features/profile/bonuses/presentation/widgets/bonus_data/prepaid_water_data_widget.dart';
 import 'package:niagara_app/features/profile/bonuses/presentation/widgets/bonus_data/temporary_bonuses_data_widget.dart';
 
 class HorizontalBonusCardsDataWidget extends StatelessWidget {
-  const HorizontalBonusCardsDataWidget({super.key});
+  const HorizontalBonusCardsDataWidget({
+    super.key,
+    required this.bonuses,
+  });
+  final Bonuses bonuses;
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        Expanded(child: BonusesDataWidget()),
-        AppBoxes.kWidth8,
-        Expanded(child: TemporaryBonusesDataWidget()),
-        AppBoxes.kWidth8,
-        Expanded(child: PrepaidWaterDataWidget()),
-      ],
+    return SizedBox(
+      height: AppSizes.kGeneral72,
+      child: Row(
+        children: [
+          const Flexible(child: BonusesDataWidget()),
+          AppBoxes.kWidth8,
+          if (bonuses.tempCount > 0) ...[
+            const Flexible(child: TemporaryBonusesDataWidget()),
+            AppBoxes.kWidth6,
+          ],
+          const Flexible(child: PrepaidWaterDataWidget()),
+        ],
+      ),
     );
   }
 }
