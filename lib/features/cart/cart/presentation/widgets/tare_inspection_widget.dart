6c08535c31dfc76ea98d4c025d1c22fbef1cea75
SergeiSMV <s9051133401@yandex.ru>
2025-03-26 15:31:26 +0300

    [NIAGARA-504] Ability to hide message.

--- lib/features/cart/cart/presentation/widgets/tare_inspection_widget.dart
+++ lib/features/cart/cart/presentation/widgets/tare_inspection_widget.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:niagara_app/core/utils/constants/app_borders.dart';
+import 'package:niagara_app/core/utils/constants/app_boxes.dart';
 import 'package:niagara_app/core/utils/constants/app_insets.dart';
 import 'package:niagara_app/core/utils/extensions/build_context_ext.dart';
 import 'package:niagara_app/core/utils/gen/strings.g.dart';
 import 'package:niagara_app/features/profile/user/presentation/bloc/user_bloc.dart';
 
 /// Виджет для проверки необходимости отображения сообщения о необходимости проверки тары.
-class TareInspectionWidget extends StatelessWidget {
+class TareInspectionWidget extends HookWidget {
   const TareInspectionWidget({super.key});
+
+  /// Метод для скрытия сообщения.
+  void _onDismiss(ValueNotifier<bool> isVisible) {
+    isVisible.value = false;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final isVisible = useState(true);
+
     return BlocBuilder<UserBloc, UserState>(
       builder: (context, state) {
         return state.maybeWhen(
           loaded: (user) {
-            return user.ordersCount == 0
-                ? const _InspectorMessageWidget()
+            return user.ordersCount > 0 && isVisible.value
+                ? _InspectorMessageWidget(
+                    onDismiss: () => _onDismiss(isVisible),
+                  )
                 : const SizedBox.shrink();
           },
           orElse: () => const SizedBox.shrink(),
@@ -29,7 +40,8 @@ class TareInspectionWidget extends StatelessWidget {
 
 /// Виджет сообщения о необходимости проверки тары.
 class _InspectorMessageWidget extends StatelessWidget {
-  const _InspectorMessageWidget();
+  const _InspectorMessageWidget({required this.onDismiss});
+  final VoidCallback onDismiss;
 
   @override
   Widget build(BuildContext context) {
@@ -42,11 +54,32 @@ class _InspectorMessageWidget extends StatelessWidget {
         ),
         child: Padding(
           padding: AppInsets.kAll16,
-          child: Text(
-            t.cart.tareInspectionMessage,
-            style: context.textStyle.textTypo.tx2Medium.copyWith(
-              color: context.colors.textColors.error,
-            ),
+          child: Column(
+            children: [
+              Text(
+                t.cart.tareInspectionMessage,
+                // textAlign: TextAlign.justify,
+                style: context.textStyle.textTypo.tx2SemiBold.copyWith(
+                  color: context.colors.textColors.error,
+                ),
+              ),
+              AppBoxes.kHeight12,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: onDismiss,
+                    child: Text(
+                      'Закрыть сообщение',
+                      textAlign: TextAlign.justify,
+                      style: context.textStyle.textTypo.tx2SemiBold.copyWith(
+                        color: context.colors.textColors.blue,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
