02739571c5f7e504edb0ec8e808104408180dcbc
SergeiSMV <s9051133401@yandex.ru>
2025-04-02 18:25:39 +0300

    [NIAGARA-511] added user data to UXCam

--- lib/core/common/presentation/router/app_router.dart
+++ lib/core/common/presentation/router/app_router.dart
@@ -1,15 +1,16 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:injectable/injectable.dart';
-import 'package:niagara_app/core/common/presentation/router/app_router.gr.dart';
-import 'package:niagara_app/core/common/presentation/router/routers/auth_routes.dart';
-import 'package:niagara_app/core/common/presentation/router/routers/cart_routes.dart';
-import 'package:niagara_app/core/common/presentation/router/routers/catalog_routes.dart';
-import 'package:niagara_app/core/common/presentation/router/routers/empty_routes.dart';
-import 'package:niagara_app/core/common/presentation/router/routers/home_routes.dart';
-import 'package:niagara_app/core/common/presentation/router/routers/location_routes.dart';
-import 'package:niagara_app/core/common/presentation/router/routers/profile_routes.dart';
-import 'package:niagara_app/core/common/presentation/router/routers/splash_routes.dart';
-import 'package:niagara_app/core/utils/gen/strings.g.dart';
+
+import '../../../utils/gen/strings.g.dart';
+import 'app_router.gr.dart';
+import 'routers/auth_routes.dart';
+import 'routers/cart_routes.dart';
+import 'routers/catalog_routes.dart';
+import 'routers/empty_routes.dart';
+import 'routers/home_routes.dart';
+import 'routers/location_routes.dart';
+import 'routers/profile_routes.dart';
+import 'routers/splash_routes.dart';
 
 /// Класс [AppRouter] роутера приложения. Содержит все модули и их маршруты.
 @AutoRouterConfig()
