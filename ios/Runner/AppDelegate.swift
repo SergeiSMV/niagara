7d473579cde4eadc5462d5b87072c41c947e4e2c
Kirill Bykov <amfeek3@gmail.com>
2025-04-25 18:15:31 +0200

    [NIAGARA-516] Удален ненужный нативный код, оставишийся от SDK

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,6 @@
 import UIKit
 import Flutter
 import YandexMapsMobile
-import UserNotifications
-import JivoSDK
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -34,42 +32,7 @@ import JivoSDK
         
         YMKMapKit.setApiKey(apiKey)
         YMKMapKit.setLocale("ru_RU")
-        
-        UNUserNotificationCenter.current().delegate = self
-        Jivo.notifications.handleLaunch(options: launchOptions)
-        
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-    
-    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-            Jivo.notifications.setPushToken(data: deviceToken)
-        }
-        
-    override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        Jivo.notifications.setPushToken(data: nil)
-    }
-        
-    override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        if let result = Jivo.notifications.didReceiveRemoteNotification(userInfo: userInfo) {
-            completionHandler(result)
-        }
-        else {
-            completionHandler(.noData)
-        }
-    }
-        
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        if let options = Jivo.notifications.willPresent(notification: notification, preferableOptions: .banner) {
-            completionHandler(options)
-        }
-        else {
-            completionHandler([])
-        }
-    }
-        
-    override func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        Jivo.notifications.didReceive(response: response)
-        completionHandler()
     }
 }
