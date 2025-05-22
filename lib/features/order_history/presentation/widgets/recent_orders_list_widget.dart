041bc1ff71e3b9d9495b3ef3d3506289e08377d5
Kirill Bykov <amfeek3@gmail.com>
2025-05-22 23:34:58 +0200

    [NIAGARA-524] Добавлена возможность обновлять список заказов на главной (pull-to-refresh)

--- lib/features/order_history/presentation/widgets/recent_orders_list_widget.dart
+++ lib/features/order_history/presentation/widgets/recent_orders_list_widget.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
+
 import '../../../../core/common/presentation/router/app_router.gr.dart';
 import '../../../../core/common/presentation/widgets/loaders/app_center_loader.dart';
 import '../../../../core/utils/constants/app_borders.dart';
@@ -16,6 +18,7 @@ import '../bloc/orders_bloc/orders_bloc.dart';
 import 'empty_orders_list_widget.dart';
 import 'order_item_widgets/order_item_widget.dart';
 
+/// Список последних заказов.
 class RecentOrdersListWidget extends StatelessWidget {
   const RecentOrdersListWidget({super.key});
 
@@ -51,33 +54,88 @@ class RecentOrdersListWidget extends StatelessWidget {
                       ),
                       SizedBox(
                         height: AppSizes.kListItemsHeight,
-                        child: SingleChildScrollView(
-                          padding: AppInsets.kHorizontal16,
-                          scrollDirection: Axis.horizontal,
-                          child: Padding(
-                            padding: AppInsets.kVertical20,
-                            child: Row(
-                              children: [
-                                ...List.generate(
-                                  firstFourOrders.length,
-                                  (index) => Padding(
-                                    padding: AppInsets.kRight12,
-                                    child: OrderItemWidget(
-                                      inHorizontalList: true,
-                                      order: firstFourOrders[index],
-                                    ),
-                                  ),
-                                ),
-                                const _AllOrdersButtonWidget(),
-                              ],
-                            ),
-                          ),
+                        child: _OrdersListWidget(
+                          firstFourOrders: firstFourOrders,
                         ),
                       ),
                     ],
                   );
           }
         },
+      );
+}
+
+/// Сам виджет списка заказов.
+class _OrdersListWidget extends StatefulWidget {
+  const _OrdersListWidget({
+    required this.firstFourOrders,
+  });
+
+  /// Первые четыре заказа.
+  final List<UserOrder> firstFourOrders;
+
+  @override
+  State<_OrdersListWidget> createState() => _OrdersListWidgetState();
+}
+
+class _OrdersListWidgetState extends State<_OrdersListWidget> {
+  /// Контроллер для обновления списка.
+  final RefreshController _refreshController = RefreshController();
+
+  /// Строит элемент списка заказов.
+  Widget _buildOrderItem(int index) => Padding(
+        padding: AppInsets.kRight12 +
+            AppInsets.kVertical20 +
+            (index == 0 ? AppInsets.kLeft16 : EdgeInsets.zero),
+        child: OrderItemWidget(
+          inHorizontalList: true,
+          order: widget.firstFourOrders[index],
+        ),
+      );
+
+  /// Строит индикатор обновления.
+  Widget _refreshIndicatorBuilder(context, state) => switch (state) {
+        RefreshStatus.refreshing => const AppCenterLoader(
+            dense: true,
+            size: AppSizes.kLoaderSmall,
+          ),
+        _ => const SizedBox.shrink(),
+      };
+
+  Future<void> _onRefresh() async {
+    context.read<OrdersBloc>().add(const OrdersEvent.loadPreview());
+  }
+
+  /// Слушает состояние загрузки заказов.
+  void _ordersLoadingStateListener(BuildContext context, OrdersState state) {
+    state.maybeWhen(
+      loaded: (_, preview) => _refreshController.refreshCompleted(),
+      error: _refreshController.refreshFailed,
+      orElse: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) => BlocListener<OrdersBloc, OrdersState>(
+        listener: _ordersLoadingStateListener,
+        child: Padding(
+          padding: EdgeInsets.zero,
+          child: SmartRefresher(
+            controller: _refreshController,
+            onRefresh: _onRefresh,
+            header: CustomHeader(builder: _refreshIndicatorBuilder),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                ...List.generate(
+                  widget.firstFourOrders.length,
+                  _buildOrderItem,
+                ),
+                const _AllOrdersButtonWidget(),
+              ],
+            ),
+          ),
+        ),
       );
 }
 
@@ -95,27 +153,30 @@ class _AllOrdersButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) => InkWell(
         onTap: () async => _goToOrders(context),
-        child: Container(
-          padding: AppInsets.kHorizontal8,
-          decoration: BoxDecoration(
-            borderRadius: AppBorders.kCircular12,
-            color: context.colors.mainColors.accent,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Assets.icons.boxOrder.svg(
-                // ignore: deprecated_member_use_from_same_package
-                color: context.colors.mainColors.white,
-              ),
-              AppBoxes.kHeight8,
-              Text(
-                t.recentOrders.allOrders,
-                textAlign: TextAlign.center,
-                style: context.textStyle.buttonTypo.btn3bold
-                    .withColor(context.colors.mainColors.white),
-              ),
-            ],
+        child: Padding(
+          padding: AppInsets.kVertical20 + AppInsets.kRight16,
+          child: Container(
+            padding: AppInsets.kHorizontal8,
+            decoration: BoxDecoration(
+              borderRadius: AppBorders.kCircular12,
+              color: context.colors.mainColors.accent,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Assets.icons.boxOrder.svg(
+                  // ignore: deprecated_member_use_from_same_package
+                  color: context.colors.mainColors.white,
+                ),
+                AppBoxes.kHeight8,
+                Text(
+                  t.recentOrders.allOrders,
+                  textAlign: TextAlign.center,
+                  style: context.textStyle.buttonTypo.btn3bold
+                      .withColor(context.colors.mainColors.white),
+                ),
+              ],
+            ),
           ),
         ),
       );
