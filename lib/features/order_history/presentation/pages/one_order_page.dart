c3c5a8c5f4a4247ca61dbbf037b5a05881849dbd
SergeiSMV <s9051133401@yandex.ru>
2025-07-31 10:55:26 +0300

    [NIAGARA-533] fixed one order page

--- lib/features/order_history/presentation/pages/one_order_page.dart
+++ lib/features/order_history/presentation/pages/one_order_page.dart
@@ -4,9 +4,11 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import '../../../../core/common/presentation/widgets/app_bar.dart';
+import '../../../../core/common/presentation/widgets/buttons/app_text_button.dart';
 import '../../../../core/common/presentation/widgets/snack_bars/app_snack_bar.dart';
 import '../../../../core/utils/constants/app_boxes.dart';
 import '../../../../core/utils/constants/app_insets.dart';
+import '../../../../core/utils/enums/order_status.dart';
 import '../../../../core/utils/extensions/build_context_ext.dart';
 import '../../../../core/utils/extensions/text_style_ext.dart';
 import '../../../../core/utils/gen/assets.gen.dart';
@@ -14,8 +16,11 @@ import '../../../../core/utils/gen/strings.g.dart';
 import '../../domain/models/user_order.dart';
 import '../bloc/rate_order_cubit/rate_order_cubit.dart';
 import '../widgets/list_products_widget.dart';
+import '../widgets/modals_widgets/cancel_order_modal_widget.dart';
+import '../widgets/modals_widgets/order_receipt_widget.dart';
+import '../widgets/modals_widgets/rate_modal_widget.dart';
+import '../widgets/modals_widgets/repeat_order_modal_widget.dart';
 import '../widgets/order_data_widget.dart';
-import '../widgets/order_item_widgets/bottom_buttons_widget.dart';
 import '../widgets/order_status_widget.dart';
 import '../widgets/prices_and_bonuses_widget.dart';
 
@@ -93,9 +98,144 @@ class OneOrderPage extends StatelessWidget {
             ListProductsWidget(products: order.products),
             BlocProvider.value(
               value: evaluateOrderCubit,
-              child: BottomButtonsWidget(order: order),
+              child: _BottomButtonsWidget(order: order),
             ),
           ],
+        ),
+      );
+}
+
+/// Кнопки внизу экрана, с учетом статуса заказа
+class _BottomButtonsWidget extends StatelessWidget {
+  const _BottomButtonsWidget({
+    required this.order,
+  });
+  final UserOrder order;
+
+  /// Открывает модальное окно с оценкой заказа
+  Future<void> _showRateModal(BuildContext context) async {
+    final cubit = context.read<RateOrderCubit>();
+    return showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      useRootNavigator: true,
+      backgroundColor: context.colors.mainColors.white,
+      useSafeArea: true,
+      builder: (ctx) => BlocProvider.value(
+        value: cubit,
+        child: RateModalWidget(orderId: order.id),
+      ),
+    );
+  }
+
+  Future<void> _showReceiptModal(BuildContext context) async =>
+      showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        useRootNavigator: true,
+        backgroundColor: context.colors.mainColors.white,
+        useSafeArea: true,
+        builder: (ctx) => OrderReceiptWidget(order: order),
+      );
+
+  Future<void> _showRepeatOrderModal(BuildContext context) async =>
+      showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        useRootNavigator: true,
+        backgroundColor: context.colors.mainColors.white,
+        useSafeArea: true,
+        builder: (ctx) => RepeatOrderModalWidget(
+          order: order,
+          outerContext: context,
+        ),
+      );
+
+  Future<void> _showCancelOrderModal(BuildContext context) async =>
+      showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        useRootNavigator: true,
+        backgroundColor: context.colors.mainColors.white,
+        useSafeArea: true,
+        builder: (ctx) => CancelOrderModalWidget(
+          order: order,
+          outerContext: context,
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) => SliverToBoxAdapter(
+        child: Padding(
+          padding: AppInsets.kHorizontal16,
+          child: Column(
+            children: [
+              AppBoxes.kHeight24,
+
+              /// Отменить заказ (status = собирается)
+              if (order.orderStatus == OrderStatus.goingTo) ...[
+                AppTextButton.secondary(
+                  text: t.recentOrders.cancelOrder,
+                  onTap: () => _showCancelOrderModal(context),
+                ),
+              ],
+
+              /// Связаться с водителем (status = в пути)
+              if (order.orderStatus == OrderStatus.onWay) ...[
+                AppTextButton.primary(
+                  text: t.recentOrders.contactDriver,
+                  onTap: () {},
+                ),
+              ],
+
+              /// Повторить заказ (status = Получен)
+              if (order.orderStatus == OrderStatus.received) ...[
+                if (order.orderAgain) ...[
+                  AppTextButton.primary(
+                    text: t.recentOrders.repeatOrder,
+                    onTap: () => _showRepeatOrderModal(context),
+                  ),
+                  AppBoxes.kHeight12,
+                ],
+
+                /// Оценить заказ
+                if (order.rating == 0) ...[
+                  BlocBuilder<RateOrderCubit, RateOrderState>(
+                    builder: (context, state) =>
+                        (state == const RateOrderState.initial() ||
+                                state == const RateOrderState.loading())
+                            ? AppTextButton.secondary(
+                                text: t.recentOrders.evaluateOrder,
+                                onTap: () => _showRateModal(context),
+                              )
+                            : const SizedBox.shrink(),
+                  ),
+                  AppBoxes.kHeight12,
+                ],
+              ],
+
+              /// Электронный чек
+              if (order.paymentCompleted) ...[
+                // AppBoxes.kHeight12,
+                AppTextButton.secondary(
+                  text: t.recentOrders.electronicReceipt,
+                  onTap: () => _showReceiptModal(context),
+                ),
+                AppBoxes.kHeight24,
+              ],
+
+              /// Повторить заказ (status = Отменен)
+              if (order.orderStatus == OrderStatus.cancelled &&
+                  order.orderAgain) ...[
+                AppBoxes.kHeight12,
+                AppTextButton.primary(
+                  text: t.recentOrders.repeatOrder,
+                  onTap: () => _showRepeatOrderModal(context),
+                ),
+                AppBoxes.kHeight24,
+              ],
+            ],
+          ),
         ),
       );
 }
